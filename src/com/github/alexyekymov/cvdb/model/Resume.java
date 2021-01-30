0faa5c8ebebc40cd5fe4c8f9b906ff859f0cdf89
Alex <alexyekymov@gmail.com>
2021-01-30 02:23:59 +0200

    add equals & hashcode

--- src/com/github/alexyekymov/cvdb/model/Resume.java
+++ src/com/github/alexyekymov/cvdb/model/Resume.java
@@ -4,16 +4,31 @@ public class Resume {
 
     String uuid;
 
-    @Override
-    public String toString() {
-        return uuid;
-    }
-
     public String getUuid() {
         return uuid;
     }
 
     public void setUuid(String uuid) {
         this.uuid = uuid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Resume resume = (Resume) o;
+
+        return uuid.equals(resume.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return uuid.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return uuid;
     }
 }
