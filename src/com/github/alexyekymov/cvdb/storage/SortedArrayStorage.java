53a5280e79a1da19f19f1435229f98fd4eccea64
Alex <alexyekymov@gmail.com>
2021-01-31 02:01:40 +0200

    add SortedArrayStorage

---
+++ src/com/github/alexyekymov/cvdb/storage/SortedArrayStorage.java
@@ -0,0 +1,39 @@
+package com.github.alexyekymov.cvdb.storage;
+
+import com.github.alexyekymov.cvdb.model.Resume;
+
+import java.util.Arrays;
+
+public class SortedArrayStorage extends AbstractArrayStorage {
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void update(Resume resume) {
+
+    }
+
+    @Override
+    public void save(Resume resume) {
+
+    }
+
+    @Override
+    public void delete(String uuid) {
+
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return new Resume[0];
+    }
+
+    @Override
+    protected int getIndex(String uuid) {
+        Resume searchKey = new Resume();
+        searchKey.setUuid(uuid);
+        return Arrays.binarySearch(storage, 0, size, searchKey);
+    }
+}
