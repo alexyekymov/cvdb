8d337b6ff111787196dc69888f057b5dce05df06
Alex <alexyekymov@gmail.com>
2021-02-02 03:18:47 +0200

    make Resume immutable

--- src/com/github/alexyekymov/cvdb/model/Resume.java
+++ src/com/github/alexyekymov/cvdb/model/Resume.java
@@ -1,15 +1,21 @@
 package com.github.alexyekymov.cvdb.model;
 
+import java.util.UUID;
+
 public class Resume implements Comparable<Resume> {
 
-    String uuid;
+    private final String uuid;
+
+    public Resume() {
+        this(UUID.randomUUID().toString());
+    }
+
+    public Resume(String uuid) {
+        this.uuid = uuid;
+    }
 
     public String getUuid() {
         return uuid;
-    }
-
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
     }
 
     @Override
