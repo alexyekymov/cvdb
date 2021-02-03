ea8501960d6b38039c17916e411d4350c0cf00d8
Alex <alexyekymov@gmail.com>
2021-02-03 16:21:33 +0200

    add tests

---
+++ test/com/github/alexyekymov/cvdb/storage/ArrayStorageTest.java
@@ -0,0 +1,7 @@
+package com.github.alexyekymov.cvdb.storage;
+
+public class ArrayStorageTest extends AbstractArrayStorageTest {
+    public ArrayStorageTest() {
+        super(new ArrayStorage());
+    }
+}
---
+++ test/com/github/alexyekymov/cvdb/storage/SortedArrayStorageTest.java
@@ -0,0 +1,8 @@
+package com.github.alexyekymov.cvdb.storage;
+
+public class SortedArrayStorageTest extends AbstractArrayStorageTest {
+
+    public SortedArrayStorageTest() {
+        super(new SortedArrayStorage());
+    }
+}
