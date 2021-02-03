761425ca33933708c3a3d4f33e25ae5137bb3fa8
Alex <alexyekymov@gmail.com>
2021-02-03 16:20:22 +0200

    reflection

---
+++ src/com/github/alexyekymov/cvdb/MainReflection.java
@@ -0,0 +1,23 @@
+package com.github.alexyekymov.cvdb;
+
+import com.github.alexyekymov.cvdb.model.Resume;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Resume resume = new Resume();
+        Class<? extends Resume> resumeClass = resume.getClass();
+        Field field = resumeClass.getDeclaredFields()[0];
+        field.setAccessible(true);
+        System.out.println(field.getName());
+        System.out.println(field.get(resume));
+        field.set(resume, "newUUID");
+
+        Method method = resumeClass.getMethod("toString");
+        Object result = method.invoke(resume);
+        System.out.println(result);
+    }
+}
