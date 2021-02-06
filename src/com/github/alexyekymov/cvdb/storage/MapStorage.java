9ec48dba29216dc258fba8cb2f8c0c9fbabf7087
Alex <alexyekymov@gmail.com>
2021-02-06 20:44:29 +0200

    MapStorage impl

---
+++ src/com/github/alexyekymov/cvdb/storage/MapStorage.java
@@ -0,0 +1,55 @@
+package com.github.alexyekymov.cvdb.storage;
+
+import com.github.alexyekymov.cvdb.model.Resume;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapStorage extends AbstractStorage {
+    Map<String, Resume> storage = new HashMap<>();
+
+    @Override
+    protected Object getSearchKey(String uuid) {
+        return uuid;
+    }
+
+    @Override
+    protected void doUpdate(Resume resume, Object searchKey) {
+        storage.computeIfPresent((String) searchKey, (key, value) -> resume);
+    }
+
+    @Override
+    protected void doSave(Resume resume, Object searchKey) {
+        storage.put((String) searchKey, resume);
+    }
+
+    @Override
+    protected void doDelete(Object searchKey) {
+        storage.remove((String) searchKey);
+    }
+
+    @Override
+    protected Resume doGet(Object searchKey) {
+        return storage.get((String) searchKey);
+    }
+
+    @Override
+    protected boolean isExist(Object searchKey) {
+        return storage.containsKey((String) searchKey);
+    }
+
+    @Override
+    public void clear() {
+        storage.clear();
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return storage.values().toArray(new Resume[0]);
+    }
+
+    @Override
+    public int size() {
+        return storage.size();
+    }
+}
