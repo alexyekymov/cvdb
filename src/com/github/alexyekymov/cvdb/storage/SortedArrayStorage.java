d22d60e40fe11785a411fd8ac0e7ab138b1bdcdb
Alex <alexyekymov@gmail.com>
2021-02-07 21:51:36 +0200

    add comparator

--- src/com/github/alexyekymov/cvdb/storage/SortedArrayStorage.java
+++ src/com/github/alexyekymov/cvdb/storage/SortedArrayStorage.java
@@ -3,12 +3,16 @@ package com.github.alexyekymov.cvdb.storage;
 import com.github.alexyekymov.cvdb.model.Resume;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
+
+    private static final Comparator<Resume> RESUME_COMPARATOR = Comparator.comparing(Resume::getUuid);
+
     @Override
     protected Integer getSearchKey(String uuid) {
         Resume searchKey = new Resume(uuid);
-        return Arrays.binarySearch(storage, 0, size, searchKey);
+        return Arrays.binarySearch(storage, 0, size, searchKey, RESUME_COMPARATOR);
     }
 
     @Override
