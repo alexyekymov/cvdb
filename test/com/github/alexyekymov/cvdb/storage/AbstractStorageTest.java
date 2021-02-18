672461c900590bfd64817ffab26dd048f1c8d4da
Alex <alexyekymov@gmail.com>
2021-02-18 14:55:44 +0200

    cleanup

--- test/com/github/alexyekymov/cvdb/storage/AbstractStorageTest.java
+++ test/com/github/alexyekymov/cvdb/storage/AbstractStorageTest.java
@@ -2,60 +2,18 @@ package com.github.alexyekymov.cvdb.storage;
 
 import com.github.alexyekymov.cvdb.exception.ExistStorageException;
 import com.github.alexyekymov.cvdb.exception.NotExistStorageException;
-import com.github.alexyekymov.cvdb.model.*;
+import com.github.alexyekymov.cvdb.model.Resume;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
 
+import static com.github.alexyekymov.cvdb.storage.ResumeTestData.*;
 import static org.junit.Assert.assertEquals;
 
 public class AbstractStorageTest {
     protected Storage storage;
-
-    private static final String UUID_1 = "uuid1";
-    private static final String UUID_2 = "uuid2";
-    private static final String UUID_3 = "uuid3";
-    private static final String UUID_4 = "uuid4";
-
-    private static final Resume R1;
-    private static final Resume R2;
-    private static final Resume R3;
-    private static final Resume R4;
-
-    static {
-        R1 = new Resume(UUID_1, "Name1");
-        R2 = new Resume(UUID_2, "Name2");
-        R3 = new Resume(UUID_3, "Name3");
-        R4 = new Resume(UUID_4, "Name4");
-
-        R1.addContact(ContactType.MAIL, "mail1@google.com");
-        R1.addContact(ContactType.PHONE, "12345");
-        R1.addSection(SectionType.OBJECTIVE, new TextSection("Objective1"));
-        R1.addSection(SectionType.PERSONAL, new TextSection("Personal data"));
-        R1.addSection(SectionType.ACHIEVEMENT, new ListSection("Achievement1", "Achievement2", "Achievement3"));
-        R1.addSection(SectionType.QUALIFICATION, new ListSection("Java", "SQL", "JS"));
-        R1.addSection(SectionType.EXPERIENCE,
-                new OrganizationSection(
-                        new Organization("Organization1", "http://organization1.com",
-                                new Organization.Position(2005, Month.JANUARY, "Position1", "content1"),
-                                new Organization.Position(2001, Month.MARCH, 2005, Month.JANUARY, "Position2", "content2")
-                        )));
-        R1.addSection(SectionType.EDUCATION,
-                new OrganizationSection(
-                        new Organization("Institute", "http://institute1.gov",
-                                new Organization.Position(1996, Month.JANUARY, 2000, Month.DECEMBER, "aspirant", null),
-                                new Organization.Position(2001, Month.MARCH, 2005, Month.JANUARY, "Student", "Computer science")),
-                        new Organization("College", "http://college1.gov")));
-        R2.addContact(ContactType.SKYPE, "skype1");
-        R2.addContact(ContactType.PHONE, "54321");
-        R2.addSection(SectionType.EXPERIENCE,
-                new OrganizationSection(
-                        new Organization("Organization2", "http://organization2.com",
-                                new Organization.Position(2015, Month.JANUARY, "position1", "content1"))));
-    }
 
     protected AbstractStorageTest(Storage storage) {
         this.storage = storage;
@@ -63,10 +21,7 @@ public class AbstractStorageTest {
 
     @Before
     public void setUp() {
-        storage.clear();
-        storage.save(R1);
-        storage.save(R2);
-        storage.save(R3);
+        refill(storage);
     }
 
     @Test
@@ -89,7 +44,7 @@ public class AbstractStorageTest {
 
     @Test(expected = NotExistStorageException.class)
     public void updateNotExist() {
-        storage.update(new Resume("dummy"));// mb get()???
+        storage.update(new Resume("dummy"));
     }
 
     @Test
---
+++ test/com/github/alexyekymov/cvdb/storage/ResumeTestData.java
@@ -0,0 +1,56 @@
+package com.github.alexyekymov.cvdb.storage;
+
+import com.github.alexyekymov.cvdb.model.*;
+
+import java.time.Month;
+
+public class ResumeTestData {
+    public static final String UUID_1 = "uuid1";
+    public static final String UUID_2 = "uuid2";
+    public static final String UUID_3 = "uuid3";
+    public static final String UUID_4 = "uuid4";
+
+    public static final Resume R1;
+    public static final Resume R2;
+    public static final Resume R3;
+    public static final Resume R4;
+
+    static {
+        R1 = new Resume(UUID_1, "Name1");
+        R2 = new Resume(UUID_2, "Name2");
+        R3 = new Resume(UUID_3, "Name3");
+        R4 = new Resume(UUID_4, "Name4");
+
+        R1.addContact(ContactType.MAIL, "mail1@google.com");
+        R1.addContact(ContactType.PHONE, "12345");
+        R1.addSection(SectionType.OBJECTIVE, new TextSection("Objective1"));
+        R1.addSection(SectionType.PERSONAL, new TextSection("Personal data"));
+        R1.addSection(SectionType.ACHIEVEMENT, new ListSection("Achievement1", "Achievement2", "Achievement3"));
+        R1.addSection(SectionType.QUALIFICATION, new ListSection("Java", "SQL", "JS"));
+        R1.addSection(SectionType.EXPERIENCE,
+                new OrganizationSection(
+                        new Organization("Organization1", "http://organization1.com",
+                                new Organization.Position(2005, Month.JANUARY, "Position1", "content1"),
+                                new Organization.Position(2001, Month.MARCH, 2005, Month.JANUARY, "Position2", "content2")
+                        )));
+        R1.addSection(SectionType.EDUCATION,
+                new OrganizationSection(
+                        new Organization("Institute", "http://institute1.gov",
+                                new Organization.Position(1996, Month.JANUARY, 2000, Month.DECEMBER, "aspirant", null),
+                                new Organization.Position(2001, Month.MARCH, 2005, Month.JANUARY, "Student", "Computer science")),
+                        new Organization("College", "http://college1.gov")));
+        R2.addContact(ContactType.SKYPE, "skype1");
+        R2.addContact(ContactType.PHONE, "54321");
+        R2.addSection(SectionType.EXPERIENCE,
+                new OrganizationSection(
+                        new Organization("Organization2", "http://organization2.com",
+                                new Organization.Position(2015, Month.JANUARY, "position1", "content1"))));
+    }
+
+    public static void refill(Storage storage) {
+        storage.clear();
+        storage.save(R1);
+        storage.save(R2);
+        storage.save(R3);
+    }
+}
