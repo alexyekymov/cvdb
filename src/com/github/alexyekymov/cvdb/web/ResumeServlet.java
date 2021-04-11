310b2c5aeb6681eff14f1f00baa5f7fbe104b3b5
Alex <alexyekymov@gmail.com>
2021-04-11 20:35:40 +0300

    servlet

--- src/com/github/alexyekymov/cvdb/web/ResumeServlet.java
+++ src/com/github/alexyekymov/cvdb/web/ResumeServlet.java
@@ -1,12 +1,27 @@
 package com.github.alexyekymov.cvdb.web;
 
+import com.github.alexyekymov.cvdb.Config;
+import com.github.alexyekymov.cvdb.model.ContactType;
+import com.github.alexyekymov.cvdb.model.Resume;
+import com.github.alexyekymov.cvdb.storage.Storage;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Writer;
 
 public class ResumeServlet extends HttpServlet {
+
+    private Storage storage;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        storage = Config.get().getStorage();
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -15,7 +30,31 @@ public class ResumeServlet extends HttpServlet {
 
         resp.setContentType("text/html; charset=UTF-8");
 
-        String name = req.getParameter("name");
-        resp.getWriter().write(name == null ? "Hello, Resumes!" : "Hello " + name + '!');
+        Writer writer = resp.getWriter();
+        writer.write(
+                "<html>\n" +
+                        "<head>\n" +
+                        "   <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n" +
+                        "    <link rel=\"stylesheet\" href=\"css/style.css\">\n" +
+                        "    <title>Список всех резюме</title>\n" +
+                        "</head>\n" +
+                        "<body>\n" +
+                        "<section>\n" +
+                        "<table border=\"1\" cellpadding=\"8\" cellspacing=\"0\">\n" +
+                        "    <tr>\n" +
+                        "        <th>Имя</th>\n" +
+                        "        <th>Email</th>\n" +
+                        "    </tr>\n");
+        for (Resume resume : storage.getAllSorted()) {
+            writer.write(
+                    "<tr>\n" +
+                            "   <td><a href=\"resume?uuid=" + resume.getUuid() + "\">" + resume.getFullName() + "</a></td>\n" +
+                            "   <td>" + resume.getContact(ContactType.MAIL) + "</td>\n" +
+                            "</tr>\n");
+        }
+        writer.write("</table>\n" +
+                "</section>\n" +
+                "</body>\n" +
+                "</html>\n");
     }
 }
