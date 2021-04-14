51430f426b56fe7e149cc725c68e4ec7bc37f813
alexyekymov <alexyekymov@gmail.com>
2021-04-14 17:49:48 +0300

    crud

--- src/com/github/alexyekymov/cvdb/web/ResumeServlet.java
+++ src/com/github/alexyekymov/cvdb/web/ResumeServlet.java
@@ -1,6 +1,8 @@
 package com.github.alexyekymov.cvdb.web;
 
 import com.github.alexyekymov.cvdb.Config;
+import com.github.alexyekymov.cvdb.model.ContactType;
+import com.github.alexyekymov.cvdb.model.Resume;
 import com.github.alexyekymov.cvdb.storage.Storage;
 
 import javax.servlet.ServletConfig;
@@ -21,8 +23,50 @@ public class ResumeServlet extends HttpServlet {
     }
 
     @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        String uuid = req.getParameter("uuid");
+        String fullName = req.getParameter("fullName");
+        Resume resume = storage.get(uuid);
+        resume.setFullName(fullName);
+        for (ContactType type : ContactType.values()) {
+            String value = req.getParameter(type.name());
+            if (value != null && value.trim().length() != 0) {
+                resume.addContact(type, value);
+            } else {
+                resume.getContacts().remove(type);
+            }
+        }
+        storage.update(resume);
+        resp.sendRedirect("resume");
+    }
+
+    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("resumes", storage.getAllSorted());
-        req.getRequestDispatcher("WEB-INF/jsp/list.jsp").forward(req, resp);
+        String uuid = req.getParameter("uuid");
+        String action = req.getParameter("action");
+        if (action == null) {
+            req.setAttribute("resumes", storage.getAllSorted());
+            req.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(req, resp);
+            return;
+        }
+
+        Resume resume;
+        switch (action) {
+            case "delete":
+                storage.delete(uuid);
+                resp.sendRedirect("resume");
+                return;
+            case "view":
+            case "edit":
+                resume = storage.get(uuid);
+                break;
+            default:
+                throw new IllegalArgumentException("Action " + action + " is illegal");
+        }
+        req.setAttribute("resume", resume);
+        req.getRequestDispatcher(
+                ("view".equals(action) ? "/WEB-INF/jsp/view.jsp" : "/WEB-INF/jsp/edit.jsp")
+        ).forward(req, resp);
     }
 }
